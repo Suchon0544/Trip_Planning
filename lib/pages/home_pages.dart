e2a73ee3c76a8aff6325afdf3c05361af1845f82
suchon.0544 <suchon.0544@gmail.com>
2022-03-15 22:43:41 +0700

    homepage update

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 // ignore: camel_case_types
 class home_page extends StatelessWidget {
@@ -7,19 +8,32 @@ class home_page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Row(
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Expanded(
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  height: 150.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('lib/assets/images/88.jpg'),
+                        fit: BoxFit.cover),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SingleChildScrollView(
             child: Container(
-              height: 200.0,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('lib/assets/images/88.jpg'),
-                    fit: BoxFit.cover),
+              margin: EdgeInsets.all(25),
+              child: Text(
+                'Trip Planner',
+                style: GoogleFonts.robotoMono(),
               ),
             ),
-          ),
+          )
         ],
       ),
     );
