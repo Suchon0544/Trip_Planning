0d406e78c275c664e0aaa3edb7bb63d55f03645e
suchon.0544 <suchon.0544@gmail.com>
2022-03-24 16:56:19 +0700

    map update

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:latlng/latlng.dart';
 
 // ignore: camel_case_types
 class map_page extends StatelessWidget {
@@ -8,7 +9,12 @@ class map_page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
-      options: MapOptions(),
+      options: MapOptions(minZoom: 10.0),
+      layers: [
+        TileLayerOptions(
+            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            subdomains: ['a', 'b', 'c'])
+      ],
     );
   }
 }
