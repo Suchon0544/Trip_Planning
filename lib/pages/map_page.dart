3e9a57786609d3b09b9fe62ad132fc6171d3368c
suchon.0544 <suchon.0544@gmail.com>
2022-03-28 15:32:53 +0700

    map update

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlng/latlng.dart';
+import 'package:latlong2/latlong.dart';
 
 // ignore: camel_case_types
 class map_page extends StatelessWidget {
@@ -9,7 +9,7 @@ class map_page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
-      options: MapOptions(minZoom: 10.0),
+      options: MapOptions(center: LatLng(14.70802, 101.41614), minZoom: 10.0),
       layers: [
         TileLayerOptions(
             urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
