e32979838d30d3ef8803b84d4bad16a60f091a7d
suchon.0544 <suchon.0544@gmail.com>
2022-03-31 15:18:51 +0700

    trip update

--- lib/pages/trip_page.dart
+++ lib/pages/trip_page.dart
@@ -6,43 +6,103 @@ class trip_page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTextStyle(
-      style: Theme.of(context).textTheme.bodyText2!,
-      child: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints viewportConstraints) {
-          return SingleChildScrollView(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                minHeight: viewportConstraints.maxHeight,
-              ),
-              child: Container(
-                padding: EdgeInsets.only(top: 10, bottom: 10),
-                child: Card(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      ListTile(
-                        leading: ClipRRect(
-                          child: Image.network(
-                            'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
-                            height: 200,
-                          ),
-                        ),
-                        title: Text('The Enchanted Nightingale'),
-                        subtitle: Text(
-                            'Music by Julie Gable. Lyrics by Sidney Stein.'),
-                        trailing: Icon(
-                          Icons.arrow_right,
-                          size: 30,
-                        ),
-                      ),
-                    ],
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  height: 150.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('lib/assets/images/88.jpg'),
+                        fit: BoxFit.cover),
                   ),
                 ),
               ),
+            ],
+          ),
+          SingleChildScrollView(
+            child: Container(
+              margin: EdgeInsets.all(25),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Trip Planner',
+                    style: TextStyle(fontWeight: FontWeight.w900, fontSize: 25),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 10, bottom: 10),
+                    child: Card(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          ListTile(
+                            leading: ClipRRect(
+                              child: Image.network(
+                                'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+                                height: 200,
+                              ),
+                            ),
+                            title: Text('The Enchanted Nightingale'),
+                            subtitle: Text(
+                                'Music by Julie Gable. Lyrics by Sidney Stein.'),
+                            trailing: Icon(
+                              Icons.arrow_right,
+                              size: 30,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'Highlight',
+                    style: TextStyle(fontWeight: FontWeight.w900, fontSize: 25),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 10, bottom: 10),
+                    child: Card(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          ListTile(
+                              leading: ClipRRect(
+                                child: Image.network(
+                                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+                                  height: 200,
+                                ),
+                              ),
+                              title: Text('The Enchanted Nightingale'),
+                              subtitle: Text(
+                                  'Music by Julie Gable. Lyrics by Sidney Stein.'),
+                              trailing: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Icon(
+                                    Icons.edit_sharp,
+                                    size: 30,
+                                  ),
+                                  Icon(
+                                    Icons.delete,
+                                    size: 30,
+                                  ),
+                                ],
+                              )),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
