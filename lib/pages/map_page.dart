3060afdc0b273f8b9bdeab1cb7cbaacdd37c6fca
suchon.0544 <suchon.0544@gmail.com>
2022-04-06 13:40:45 +0700

    map

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:latlong2/latlong.dart';
 
 // ignore: camel_case_types
@@ -9,7 +10,13 @@ class map_page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
-      options: MapOptions(center: LatLng(14.70802, 101.41614), minZoom: 10.0),
+      options: MapOptions(
+        center: LatLng(14.70802, 101.41614),
+        minZoom: 10.0,
+        plugins: [
+          MarkerClusterPlugin(),
+        ],
+      ),
       layers: [
         TileLayerOptions(
             urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
